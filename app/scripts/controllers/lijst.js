d9f870fbbb692e83dfce58b849a4a6284ef64059
Pieter Pletinckx <pieplay@gmail.com>
2015-08-25 09:18:56 +0200

    Untested filterfix

--- app/scripts/controllers/lijst.js
+++ app/scripts/controllers/lijst.js
@@ -10,6 +10,7 @@
 angular.module('angularZomer2015App')
   .controller('LijstCtrl', function ($scope,nodePieter,netNico) {
 $scope.filterForm={
+  title:"",
   age:true,
   vakantie:"zomervakantie"
 };
@@ -22,11 +23,13 @@ $scope.filterForm={
     });
 
     $scope.filterFunction = function(element) {
+      var filter = $scope.filterform;
       console.log("Debug voor filter");
       console.log(element.leeftijd.min_leeftijd+" < "+ $scope.filterForm.age +" < "+ element.leeftijd.max_leeftijd);
-    if($scope.filterForm.age==true) return true;
-    return element.leeftijd.min_leeftijd<=$scope.filterForm.age &&
-     $scope.filterForm.age <= element.leeftijd.max_leeftijd;
+    if($scope.filterForm.age==true && filter.title.length()<1) return true;
+
+    return (element.leeftijd.min_leeftijd<=$scope.filterForm.age &&
+     $scope.filterForm.age <= element.leeftijd.max_leeftijd) && (element.titel.test(filter.title));
   };
 
   });
