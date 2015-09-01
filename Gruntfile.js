f5f5c4384e6e27bc62c02140a0486b30f10cdb87
Pieter Pletinckx <pieplay@gmail.com>
2015-09-01 19:12:50 +0200

    CI: Grunt test, don't stay open

--- Gruntfile.js
+++ Gruntfile.js
@@ -384,7 +384,7 @@ module.exports = function (grunt) {
     karma: {
       unit: {
         configFile: 'test/karma.conf.js',
-        singleRun: false
+        singleRun: true
       }
     }
   });
